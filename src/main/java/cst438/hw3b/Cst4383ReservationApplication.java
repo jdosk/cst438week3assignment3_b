b79f8c9e036a59976602570a5f0ea5d774333861
Jdosk <jdoskocil86@gmail.com>
2020-07-12 13:52:52 -0700

    Initial commit

---
+++ src/main/java/cst438/hw3b/Cst4383ReservationApplication.java
@@ -0,0 +1,13 @@
+package cst438.hw3b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cst4383ReservationApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Cst4383ReservationApplication.class, args);
+  }
+
+}
